3e4b7d0f042343324c6f37115b9d882b564ff745
Christian Reisinger <reisinger@itp.uni-frankfurt.de>
2019-11-28 19:05:48 +0100

    Add util.h/c and function random_unsigned_from_zero_to_max

---
+++ include/util.h
@@ -0,0 +1,6 @@
+#ifndef INCLUDE_UTIL_H_
+#define INCLUDE_UTIL_H_
+
+unsigned random_unsigned_from_zero_to_max(unsigned max);
+
+#endif
---
+++ src/util.c
@@ -0,0 +1,14 @@
+#include <time.h>
+#include <stdbool.h>
+
+#include <util.h>
+
+unsigned random_unsigned_from_zero_to_max(unsigned max) {
+	static bool rand_needs_init = true;
+	if (rand_needs_init) {
+		srand(time(NULL));
+		rand_needs_init = false;
+	}
+
+	return max == 0 ? 0 : ((unsigned) rand()) % max;
+}
