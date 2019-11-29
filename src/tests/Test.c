a564736589ae9b484d953f5050acedfb2161ed95
Christian Reisinger <reisinger@itp.uni-frankfurt.de>
2019-11-29 16:39:04 +0100

    Add test_report in Test.c

--- src/tests/Test.c
+++ src/tests/Test.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 #include <tests/board.h>
+
+void test_report(char* test_name, unsigned* passed, unsigned* failed, bool success) {
+	printf("%-30s ... ", test_name);
+	if (success) {
+		printf("success\n");
+		++(*passed);
+	} else {
+		++(*failed);
+		printf("failed\n");
+	}
+}
 
 int main(int argc, char **argv) {
 	printf("Running tests ...\n");
 
 	unsigned passed = 0, failed = 0;
-	test_init_board(12, 17, 23) ? ++passed : ++failed;
-	test_place_mines(12, 17, 23, 500000, 0.9) ? ++passed : ++failed;
+	test_report("init_board", &passed, &failed, test_init_board(12, 17, 23));
+	test_report("place_mines", &passed, &failed, test_place_mines(12, 17, 23, 100000, 0.90));
 
 	printf("%u tests run, %u passed, %u failed\n", passed + failed, passed, failed);
 }
