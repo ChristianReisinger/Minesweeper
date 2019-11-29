192159d3ba4868649e7c9bac9c649066c3a6ce1e
Christian Reisinger <reisinger@itp.uni-frankfurt.de>
2019-11-30 00:40:12 +0100

    Rename macros.h > defs.h & add DEFAULT_MINE_FRACTION

--- include/defs.h
+++ include/defs.h
@@ -1,7 +1,10 @@
-#ifndef INCLUDE_MACROS_H_
-#define INCLUDE_MACROS_H_
+#ifndef INCLUDE_DEFS_H_
+#define INCLUDE_DEFS_H_
 
+
+#define DEFAULT_MINE_FRACTION 0.1
 #define DEBUG=1
+
 #define debug_print(...) \
             do { if (DEBUG) fprintf(stderr, __VA_ARGS__); } while (0)
 
