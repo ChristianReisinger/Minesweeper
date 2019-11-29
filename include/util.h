9cab987af0cdfe876de6b8bf8f4e8d79b17b9419
Christian Reisinger <reisinger@itp.uni-frankfurt.de>
2019-11-29 22:35:33 +0100

    Move error to correct file

--- include/util.h
+++ include/util.h
@@ -1,6 +1,12 @@
 #ifndef INCLUDE_UTIL_H_
 #define INCLUDE_UTIL_H_
 
+typedef enum {
+	SUCCESS,
+	MEMORY_ERR,
+	OVERFLOW_ERR
+} error;
+
 unsigned random_unsigned_from_zero_to_max(unsigned max);
 
 #endif
