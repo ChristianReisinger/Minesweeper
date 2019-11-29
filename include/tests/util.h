0e47c7963cbb405dbef1ba60d5e811515b26255e
Christian Reisinger <reisinger@itp.uni-frankfurt.de>
2019-11-29 20:42:11 +0100

    Implement test_random_unsigned_from_zero_to_max

--- include/tests/util.h
+++ include/tests/util.h
@@ -1,6 +1,8 @@
+#include <stdbool.h>
+
 #ifndef INCLUDE_TESTS_UTIL_H_
 #define INCLUDE_TESTS_UTIL_H_
 
-test_random_unsigned_from_zero_to_max(unsigned num_draws);
+bool test_random_unsigned_from_zero_to_max(unsigned max, unsigned num_draws, double allowed_relative_probability_deviation);
 
 #endif
--- src/tests/util.c
+++ src/tests/util.c
@@ -4,23 +4,20 @@
 
 #include <tests/util.h>
 
-test_random_unsigned_from_zero_to_max(unsigned max, unsigned num_draws) {
-	for (unsigned curr_max = 0; curr_max <= max; ++curr_max) {
+bool test_random_unsigned_from_zero_to_max(unsigned max, unsigned num_draws, double allowed_relative_probability_deviation) {
+	unsigned draws[max + 1];
+	for (int i = 0; i <= max; ++i)
+		draws[i] = 0;
 
-		unsigned draws[curr_max + 1];
-		for (int i = 0; i <= curr_max; ++i)
-			draws[i] = 0;
+	for (unsigned i = 0; i <= num_draws; ++i)
+		++draws[random_unsigned_from_zero_to_max(max)];
 
-		for (unsigned i = 0; i < num_draws; ++i)
-			++draws[random_unsigned_from_zero_to_max(curr_max)];
-
-		double sigma = 0.0;
-		double expected_draws = (1.0 / (curr_max + 1));
-		for (int i = 0; i <= curr_max; ++i) {
-			const unsigned avg_draws = ((double) draws[i]) / (curr_max + 1);
-			sigma += (avg_draws - expected_draws) * (avg_draws - expected_draws);
-		}
-		sigma = sqrt(sigma / (curr_max + 1));
-
+	double expected_avg_draws = (1.0 / (max + 1));
+	for (int i = 0; i <= max; ++i) {
+		const double avg_draws = ((double) draws[i]) / num_draws;
+		if (avg_draws < expected_avg_draws * (1 - allowed_relative_probability_deviation) ||
+				avg_draws > expected_avg_draws * (1 + allowed_relative_probability_deviation))
+			return false;
 	}
+	return true;
 }
