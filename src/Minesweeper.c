4f56a70aa6bacd00d7b5c52e9fe529d4f02bc74d
Christian Reisinger <reisinger@itp.uni-frankfurt.de>
2019-11-30 01:05:11 +0100

    Add default board to main

--- src/Minesweeper.c
+++ src/Minesweeper.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 
+#include <defs.h>
 #include <command_line.h>
+#include <util.h>
+#include <board.h>
+#include <geometry_rect.h>
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
 
 	handle_GNU_options(argc, argv);
+
+	board_geometry g = make_geometry(DEFAULT_ROW_NUM, DEFAULT_COL_NUM);
+	board* minesweeper_board;
+	allocate_board(&minesweeper_board, &g, get_tile_num(&g) * DEFAULT_MINE_FRACTION);
+
+	free_board(&minesweeper_board);
+
 	return 0;
 }
