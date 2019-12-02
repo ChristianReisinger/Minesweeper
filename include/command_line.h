898ca15cbd46b21dd834f9ce4ed9f776acef4e9e
Christian Reisinger <reisinger@itp.uni-frankfurt.de>
2019-12-02 15:06:23 +0100

    Rename print_help > print_program_help and add print_game_help

--- include/command_line.h
+++ include/command_line.h
@@ -1,7 +1,8 @@
 #ifndef INCLUDE_COMMAND_LINE_C_
 #define INCLUDE_COMMAND_LINE_C_
 
-void print_help(char* argv0);
+void print_program_help(char* argv0);
+void print_game_help();
 void handle_GNU_options(int argc, char** argv);
 
 #endif
--- src/command_line.c
+++ src/command_line.c
@@ -3,8 +3,19 @@
 
 #include <command_line.h>
 
-void print_help(char* argv0) {
+void print_program_help(char* argv0) {
 	printf("Usage: %s\n", argv0);
+}
+
+void print_game_help() {
+	printf("Possible user input:\n"
+			"\t%-15s : reveal tile at (row,col)\n"
+			"\t%-15s : arm tile at (row,col)\n"
+			"\t%-15s : disarm tile at (row,col)\n"
+			"\t%-15s : print this help text\n",
+			"\t%-15s : quit the game\n"
+			"r on an already reveiled tile reveils adjacent tiles if it is safe!",
+			"r row col", "a row col", "d row col", "h", "q");
 }
 
 void handle_GNU_options(int argc, char** argv) {
@@ -17,7 +28,7 @@ void handle_GNU_options(int argc, char** argv) {
 	while ((opt = getopt_long(argc, argv, "h", long_opts, &long_opts_i)) != -1) {
 		switch (opt) {
 			case 'h':
-				print_help(argv[0]);
+				print_program_help(argv[0]);
 			break;
 		}
 	}
