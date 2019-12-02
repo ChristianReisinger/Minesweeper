b573e847e3315c7ca6004ce080016aa46de75e68
Christian Reisinger <reisinger@itp.uni-frankfurt.de>
2019-12-02 15:05:22 +0100

    Add coords, separators to printed board & make tiles larger

--- src/console_ui_rect.c
+++ src/console_ui_rect.c
@@ -1,30 +1,66 @@
 #include <stdio.h>
+#include <string.h>
 
 #include <console_ui_rect.h>
 
-char get_state_char(int state) {
+static void get_state_char(int state, char* buf, unsigned buf_size) {
+	if (buf == NULL || buf_size < 4)
+		return;
+
 	if (state == STATE_ARMED)
-		return '!';
+		strncpy(buf, " ! ", buf_size);
 	else if (state == STATE_HIDDEN)
-		return 'O';
+		strncpy(buf, " # ", buf_size);
 	else if (state == STATE_MINE)
-		return 'X';
+		strncpy(buf, " X ", buf_size);
 	else if (state == STATE_MINE_MARKED)
-		return '+';
+		strncpy(buf, " + ", buf_size);
 	else if (state == 0)
-		return ' ';
-	else if (state > 0 && state < 9)
-		return '0' + state;
+		strncpy(buf, "   ", buf_size);
+	else if (state > 0 && state < 9) {
+		strncpy(buf, "   ", buf_size);
+		buf[1] = '0' + state;
+	}
+}
+
+static void print_centered_3digit(unsigned i) {
+	if (i < 10)
+		printf(" %d ", i);
 	else
-		return '?';
+		printf("%03d", i);
+}
+
+static void print_row_sep(const board_geometry* g) {
+	printf("    =");
+	for (unsigned col = 0; col < g->num_cols; ++col)
+		printf("====");
+	printf("\n");
+}
+
+static void print_col_header(const board_geometry* g) {
+	printf("    ");
+	for (unsigned col = 0; col < g->num_cols; ++col) {
+		printf(" ");
+		print_centered_3digit(col);
+	}
+	printf("\n");
 }
 
 void print_board(const board* b, const board_geometry* g) {
-	for (int row = 0; row < g->num_rows; ++row) {
-		for (int col = 0; col < g->num_cols; ++col) {
-			printf("%c", get_state_char(b->state[get_index(row, col, g)]));
-
+	print_col_header(g);
+	print_row_sep(g);
+	for (unsigned row = 0; row < g->num_rows; ++row) {
+		print_centered_3digit(row);
+		printf(" |");
+		for (unsigned col = 0; col < g->num_cols; ++col) {
+			char state_str[4];
+			get_state_char(b->state[get_index(row, col, g)], state_str, sizeof(state_str));
+			printf("%s|", state_str);
 		}
+		printf(" ");
+		print_centered_3digit(row);
 		printf("\n");
+		print_row_sep(g);
 	}
+	print_col_header(g);
 }
