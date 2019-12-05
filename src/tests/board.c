04a5664581fd921a2ff1b64f3ab2716724db4f5f
Christian Reisinger <reisinger@itp.uni-frankfurt.de>
2019-12-05 18:23:23 +0100

    Update board tests

--- src/tests/board.c
+++ src/tests/board.c
@@ -9,32 +9,49 @@
 
 #include <tests/board.h>
 
+static bool create_setup(game_setup** setup) {
+	debug_print("Creating setup ... ");
+	if (alloc_game_setup(setup) != SUCCESS)
+		return false;
+	get_default_setup(*setup);
+	debug_print("o.k.\n");
+	return true;
+}
+
+static bool create_geometry(board_geometry** g) {
+	debug_print("Creating geometry ... ");
+	if (alloc_geometry(g) != SUCCESS || init_default_geometry(*g) != SUCCESS)
+		return false;
+	debug_print("o.k.\n");
+	return true;
+}
+
+static bool create_board(board** b, const board_geometry* g, const game_setup* setup) {
+	debug_print("Creating board with %d tiles ... ", get_tile_num(g));
+	if (allocate_board(b, g) != SUCCESS || init_board(*b, setup) != SUCCESS)
+		return false;
+	debug_print("o.k.\n");
+	return true;
+}
+
 bool test_init_board() {
-	debug_print("Creating setup ...\n");
 	game_setup* setup;
-	if (alloc_game_setup(&setup) != SUCCESS)
-		return false;
-	get_default_setup(setup);
-	const unsigned num_mines = get_mine_num(setup);
-
-	debug_print("Creating geometry ...\n");
 	board_geometry* g;
-	if (alloc_geometry(&g) != SUCCESS || init_default_geometry(g) != SUCCESS)
-		return false;
-
-	unsigned num_tiles = get_tile_num(g);
-
-	debug_print("Creating board ...\n");
 	board* b;
-	if (allocate_board(&b, g) != SUCCESS || init_board(b, setup) != SUCCESS)
+
+	if (!create_setup(&setup)
+			|| !create_geometry(&g)
+			|| !create_board(&b, g, setup))
 		return false;
 
-	for (unsigned i = 0; i < num_tiles; ++i)
+	debug_print("Verifying board ... ");
+	if (b->num_tiles != get_tile_num(g) || b->num_mines != get_mine_num(setup) || b->mines_placed)
+		return false;
+
+	for (unsigned i = 0; i < get_tile_num(g); ++i)
 		if (b->mined[i] || b->state[i] != STATE_HIDDEN)
 			return false;
-
-	if (b->num_tiles != num_tiles || b->num_mines != num_mines || b->mines_placed)
-		return false;
+	debug_print("o.k.\n");
 
 	free_board(&b);
 	free_geometry(&g);
@@ -46,26 +63,19 @@ bool test_init_board() {
 }
 
 bool test_place_mines(unsigned num_runs, double allowed_relative_probability_deviation) {
-	debug_print("Allocating game_setup ...\n");
 	game_setup* setup;
-	if (alloc_game_setup(&setup) != SUCCESS)
+	board_geometry* g;
+	board* b;
+
+	if (!create_setup(&setup)
+			|| !create_geometry(&g)
+			|| !create_board(&b, g, setup))
 		return false;
-	get_default_setup(setup);
+
+	const unsigned num_tiles = get_tile_num(g);
 	const unsigned num_mines = get_mine_num(setup);
 
-	debug_print("Allocating geometry ...\n");
-	board_geometry* g;
-	if (alloc_geometry(&g) != SUCCESS || init_default_geometry(g) != SUCCESS)
-		return false;
-
-	unsigned num_tiles = get_tile_num(g);
-
-	debug_print("Allocating board with %d tiles ...\n", num_tiles);
-	board* b;
-	if ((allocate_board(&b, g)) != SUCCESS || init_board(b, setup) != SUCCESS)
-		return false;
-
-	debug_print("Verifying mine number ...\n");
+	debug_print("Verifying mine number ... ");
 	reveil(b, g, 0);
 	unsigned placed_mines = 0;
 	for (unsigned i = 0; i < num_tiles; ++i)
@@ -73,12 +83,13 @@ bool test_place_mines(unsigned num_runs, double allowed_relative_probability_dev
 			++placed_mines;
 	if (placed_mines != num_mines)
 		return false;
+	debug_print("o.k.\n");
 
 	unsigned total_placed_mines[num_tiles];
 	for (unsigned i = 0; i < num_tiles; ++i)
 		total_placed_mines[i] = 0;
 
-	debug_print("Sampling average number of placed mines ...\n");
+	debug_print("Sampling average number of placed mines ... ");
 	for (unsigned n = 0; n < num_runs; ++n) {
 		init_board(b, setup);
 		reveil(b, g, 0); /* (x) board_index = 0 will never have a mine ! */
@@ -86,15 +97,24 @@ bool test_place_mines(unsigned num_runs, double allowed_relative_probability_dev
 			if (b->mined[i])
 				++total_placed_mines[i];
 	}
+	debug_print("o.k.\n");
 
-	debug_print("Verifying probability distribution ...\n");
+	debug_print("Total mines placed: ");
+	for (unsigned i = 0; i < num_tiles; ++i)
+		debug_print("%d ", total_placed_mines[i]);
+	debug_print("\n");
+
+	debug_print("Verifying probability distribution ... ");
 	const double expected_avg_mines = ((double) num_mines / (num_tiles - 1.0)); /* (x) */
 	for (unsigned i = 1; i < num_tiles; ++i) { /* (x) */
 		const double avg_mines = ((double) total_placed_mines[i] / num_runs);
 		if (avg_mines < expected_avg_mines * (1 - allowed_relative_probability_deviation)
-				|| avg_mines > expected_avg_mines * (1 + allowed_relative_probability_deviation))
+				|| avg_mines > expected_avg_mines * (1 + allowed_relative_probability_deviation)) {
+			debug_print("unexpected probability at tile %u: %lf, expected %lf\n", i, avg_mines, expected_avg_mines);
 			return false;
+		}
 	}
+	debug_print("o.k.\n");
 
 	free_board(&b);
 	free_geometry(&g);
