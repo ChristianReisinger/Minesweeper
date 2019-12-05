58bfe04d9ee3f96d7d041abe4c8487106dad7375
Christian Reisinger <reisinger@itp.uni-frankfurt.de>
2019-12-05 18:20:51 +0100

    Add flush in debug_print

--- include/defs.h
+++ include/defs.h
@@ -6,6 +6,6 @@
 #define DEBUG 1
 
 #define debug_print(...) \
-            do { if (DEBUG) fprintf(stderr, __VA_ARGS__); } while (0)
+            do { if (DEBUG) fprintf(stderr, __VA_ARGS__); fflush(stderr); } while (0)
 
 #endif
