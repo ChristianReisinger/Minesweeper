68c61f1e5223d867496d75da2addcc7c82f46f86
Christian Reisinger <reisinger@itp.uni-frankfurt.de>
2019-12-12 16:20:09 +0100

    Add //TODO's

--- include/geometry/console_ui.h
+++ include/geometry/console_ui.h
@@ -7,8 +7,8 @@
 #ifndef INCLUDE_GEOMETRY_CONSOLE_UI_H_
 #define INCLUDE_GEOMETRY_CONSOLE_UI_H_
 
-void print_header(const board* b, const board_geometry* g, const time_t game_start_time);
+void print_header(const board* b, const board_geometry* g, const time_t game_start_time); //TODO pass width instead of geometry and put into more general header
 void print_board(const board* b, const board_geometry* g);
-bool handle_action(bool* lost, board* b, const board_geometry* g, char action);
+bool handle_action(bool* lost, board* b, const board_geometry* g, char action); //TODO make only coordinate parsing geometry-specific and put into more general header
 
 #endif
